0cc5eab43491bc86615b4f01a83c3679b35cb652
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-01-05 23:29:22 +0900

    シェルスクリプトを実装
    (1) 作業ディレクトリを初期化するスクリプト

--- initwd.sh
+++ initwd.sh
@@ -1,3 +1,13 @@
 #!/bin/bash  -x
 
 set  -ue
+
+project=$1
+trg_dir=${2:-"${project}"}
+
+sed -e "s/@REPO_NAME@/${project}" ./template/hgrc.main.in   \
+  | tee "${project}/hgrc.main"
+sed -e "s/@REPO_NAME@/${project}" ./template/hgrc.conv.in   \
+  | tee "${project}/hgrc.conv"
+sed -e "s/@REPO_NAME@/${project}" ./template/run-hg-convert.sh.in   \
+  | tee "${project}/run-hg-convert.sh"
