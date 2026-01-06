192484426b78169f84e1781d0a26d587a396ac76
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-01-06 12:46:49 +0900

    不具合修正

--- initwd.sh
+++ initwd.sh
@@ -5,9 +5,9 @@ set  -ue
 project=$1
 trg_dir=${2:-"${project}"}
 
-sed -e "s/@REPO_NAME@/${project}" ./template/hgrc.main.in   \
+sed -e "s:@REPO_NAME@:${project}:g" .template/hgrc.main.in  \
   | tee "${project}/hgrc.main"
-sed -e "s/@REPO_NAME@/${project}" ./template/hgrc.conv.in   \
+sed -e "s:@REPO_NAME@:${project}:g" .template/hgrc.conv.in  \
   | tee "${project}/hgrc.conv"
-sed -e "s/@REPO_NAME@/${project}" ./template/run-hg-convert.sh.in   \
+sed -e "s:@REPO_NAME@:${project}:g" .template/run-hg-convert.sh.in  \
   | tee "${project}/run-hg-convert.sh"
